4fc94582444873bf1240d2605f75be6433eb88a9
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-20 09:08:35 +0530

    Variations of Event Handlers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            {/* <button onClick={() => console.log('Button was clicked!')}>Add Animal</button> */}
+            {/* <button
+                onClick={function () {
+                    console.log('Button was clicked!');
+                }}
+            >
+                Add Animal
+            </button> */}
         </div>
     );
 }
