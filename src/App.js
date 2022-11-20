c420eafae28047f33fa6d2383d6fbda1757b4874
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-20 09:49:35 +0530

    Why Array Destructuring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from 'react';
 
 function App() {
-    const [count, setCount] = useState(0);
+    function makeArray() {
+        return [1, 10, 32, 40];
+    }
 
-    const handleClick = () => {
-        setCount(count + 1);
-    };
+    // const myArray = makeArray();
+    // const firstElement = myArray[0];
+    // const secondElement = myArray[1];
 
-    return (
-        <div>
-            <button onClick={handleClick}>Add Animal</button>
-            <div>Number of animals: {count}</div>
-        </div>
-    );
+    const [firstElement, secondElement] = makeArray();
+
+    console.log(firstElement, secondElement);
+
+    // const [count, setCount] = useState(0);
+
+    // const handleClick = () => {
+    //     setCount(count + 1);
+    // };
+
+    // return (
+    //     <div>
+    //         <button onClick={handleClick}>Add Animal</button>
+    //         <div>Number of animals: {count}</div>
+    //     </div>
+    // );
 }
 
 export default App;
