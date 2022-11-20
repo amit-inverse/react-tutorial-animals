eeb3bfca8a1330fd9df8e137847d6177e71e40b8
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-20 08:03:01 +0530

    Introducing the Event System

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-    return <div>Show Animal List Here!</div>;
+    const handleClick = () => {
+        console.log('Button was clicked!');
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add Animal</button>
+        </div>
+    );
 }
 
 export default App;
